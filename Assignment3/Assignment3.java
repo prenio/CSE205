4a7b170bf689d345f651734be45572bad39dcf63
Pierce Renio <62824197+prenio@users.noreply.github.com>
2020-12-31 13:48:57 -0700

    Add files via upload

---
+++ Assignment3/Assignment3.java
@@ -0,0 +1,55 @@
+Assignment #: 3
+        Name: Pierce Renio
+   StudentID: 1214793827
+     Lecture: MWF 9:40-10:30
+
+1. Thou shalt not use a computer to harm other people.
+
+-- Using a computer to lower someone's bank account balance
+without a proper transaction and authorization can harm that
+person financially. This type of actions should not be allowed.
+
+.....
+
+and so on.
+
+(Don't use the above example. You need to have your own examples. Also make
+sure your file name must be Assignment3.java, cannot be Assignment3.java.txt,etc)
+
+
+
+1.3 Be honest and trustworthy.
+
+-- Using someone else's code and claiming it as your own. Code should not 
+	be copied like this as it is plagarism. Sourcing the owner of the code is 
+	the right thing to do if it is acceptable.  
+	
+	
+1.4 Be fair and take action not to discriminate.
+
+--If you are offering help to people, do not deny people who may be from 
+a particular age, race, gender, etc. Help everyone and don't discriminate.
+
+
+2.6 Perform work only in areas of competence.
+
+--Do not work somewhere that you do not have the necessary experience for. Example, 
+if the job requires knowledge of Java, but you only know Python, it is important to disclose 
+this information.
+
+
+3.1 Ensure that the public good is the central concern during all professional computing work.
+
+--I should not code with selfish intentions. I should code for the sake of the public good as 
+it is the central concern.
+
+
+4.1 Uphold, promote, and respect the principles of the Code.
+
+--If I see a friend/peer attempting to violate the principles of the Code, I should
+take responsible action and either disuade them of doing to violation, or reporting to an authoritative figure.
+
+
+
+
+
